fcc12fb797e343656f4ec88a7ec6bb259bced994
dracuxan <khodkenisarg@gmail.com>
2025-03-26 21:38:57 +0530

    upd

--- pointers.c
+++ pointers.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 int main() {
+  // char a = 'a';
+  // char b = 'a';
+  //
+  // printf("%p\n", &a);
+  // printf("%p\n", &b);
 
-  char a = 'a';
-  char b = 'a';
-
-  printf("%p\n", &a);
-  printf("%p\n", &b);
+  int x = 0;
+  int *x_ptr = &x;
+  *x_ptr = 1;
+  printf("%d\n", x);
+  printf("%d\n", *x_ptr);
 
   return EXIT_SUCCESS;
 }
