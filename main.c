67a133704f770096dd88490ddfe39e97a37e2143
dracuxan <nisargkhodke@gmail.com>
2025-08-25 01:37:05 +0530

    upd

--- main.c
+++ main.c
@@ -1,13 +1,48 @@
-#include "main.h"
+#include "strings.h"
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-  printf("Hello World!\n");
+#define BUFFSIZE 150
 
-  if (argc > 1) {
-    printf("%s\n", argv[1]);
-  } else {
-    printf("No arguments supplied.\n");
+void err_sys(const char *x) {
+  perror(x);
+  exit(1);
+}
+
+int main(int argc, char **argv) {
+  struct sockaddr_in servaddr;
+  char recv_bff[BUFFSIZE];
+  int sockfd, n;
+
+  if (argc != 2)
+    err_sys("usage: main <IP_ADDRESS>");
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    err_sys("socket error");
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(13);
+  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+    err_sys("inet_pton error for the given input");
+  }
+  if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+    err_sys("connect error");
   }
 
-  return EXIT_SUCCESS;
+  while ((n = read(sockfd, recv_bff, BUFFSIZE)) > 0) {
+    recv_bff[n] = 0;
+    if (fputs(recv_bff, stdout) == EOF) {
+      err_sys("fputs error");
+    }
+  }
+  if (n < 0) {
+    err_sys("read error");
+  }
+
+  exit(0);
 }
--- main.h
+++
@@ -1,3 +0,0 @@
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
